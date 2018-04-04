d538185a03f2e57dd0a6ae6d9ea2b1b4dc5f2fa3
Wesam Herbawi <wesam.herbawi@gmail.com>
2018-04-04 10:52:12 +0200

    generic method

--- src/main/java/edu/bu/runners/ComparablesTester.java
+++ src/main/java/edu/bu/runners/ComparablesTester.java
@@ -3,29 +3,30 @@ package edu.bu.runners;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils.Collections;
+
 import edu.bu.domain.Comparables;
 import edu.bu.domain.House;
 
 public class ComparablesTester {
 	public static void main(String[] a) {
-		List<Comparables<House>> houses = new ArrayList<>();
+		List<House> houses = new ArrayList<>();
 		houses.add(new House(10));
 		houses.add(new House(20));
 
-		List<Comparables<Car>> cars = new ArrayList<>();
+		List<Car> cars = new ArrayList<>();
 		cars.add(new Car(50));
 		cars.add(new Car(440));
-
+		java.util.Collections.sort(new ArrayList<String>());
 		Object max = getMax(cars);
-
-		// Object max2 = getMax(houses);
-		// System.out.println(max2);
+		Object max2 = getMax(houses);
+		System.out.println(max2);
 		System.out.println(max);
 	}
 
-	private static Object getMax(List<Comparables<Car>> cars) {
-		Comparables max = null;
-		for (Comparables comparable : cars) {
+	private static <T extends Comparables<T>> Comparables<T> getMax(List<T> cars) {
+		Comparables<T> max = null;
+		for (T comparable : cars) {
 			if (max == null || max.compare(comparable) < 0)
 				max = comparable;
 		}
